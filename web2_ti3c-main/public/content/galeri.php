88a720b572438242f9bac79780cce2055fb7c624
lasyahrul2@gmail.com <lasyahrul2@gmail.com>
2023-02-15 00:49:55 +0700

    first commit

---
+++ web2_ti3c-main/public/content/galeri.php
@@ -0,0 +1,65 @@
+
+<header class="container bg-success text-white">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1 class="display-2">Galleri </h1>
+                <p class="lead">Pelatihan dan bursa karir</p>
+            </div>
+        </div>
+    </header>
+    <main class="container border p-md-5 p-2">
+        <div class="row g-2 g-md-4">
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="foto/database.jpg" alt="poster">
+            </div>
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="foto/laravel.jpeg" alt="poster">
+            </div>
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="foto/PHOTOSHOP.png" alt="poster">
+            </div>
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="foto/Microsoft-Excel.png" alt="poster">
+            </div>
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="foto/mobile_apps.png" alt="training">
+            </div>
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="foto/instalasi_windows.jpg" alt="pelatihan">
+            </div>
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="foto/fotografi.jpg" alt="pelatihan">
+            </div>
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="foto/PELATIHAN-KARIR.png" alt="pelatihan">
+            </div>
+        </div>
+    </main>
+
+    <main class="container border">
+        <div class="row">
+        <div class="col-md-4 py-5">
+                <img class="w-100" src="foto/nobarfilm.jpeg" alt="NOBAR">
+            </div>
+            <div class="col-md-8 py-5">
+                <h1>NOBAR FILM INSPIRATIF "The Pursuit Of Happyness"</h1>
+                <p>
+                    Tujuan diadakannya kegiatan nonton bareng Film adalah sebagai pendekatan komunikatif yang 
+                    sangat baik dalam dunia pendidikan khususnya terhadap pelajar. selain itu juga kegiatan ini 
+                    dapat memberikan inspirasi kepada mahasiswa, mengasah keterampilan analisis mahasiswa serta 
+                    dapat meningkatkan motivasi diri.
+                    Proses internalisasi nilai melalui film diharapkan dapat menciptakan karakter positif melalui 
+                    karakter-karakter baik yang diperankan dalam film yang bersangkutan sehingga dapat menjadi inspirasi
+                    bagi pelajar khususnya mahasiswa ITB Stikom Ambon.
+                </p>
+            </div>
+        </div>
+    </main>
+
+    <footer class="container bg-light">
+        <div class="row">
+            <div class="col-12 py-4">
+                &copy; 2023 galeri
+            </div>
+        </div>
+    </footer>
